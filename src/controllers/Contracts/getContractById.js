0a29f3f7e7601c512e89329e65d9033b5dc11231
RudeusGrayrat <miguelnc2502@gmail.com>
2025-12-11 09:21:11 -0500

    get contract verify

--- src/controllers/Contracts/getContractById.js
+++ src/controllers/Contracts/getContractById.js
@@ -3,14 +3,16 @@ const Contract = require("../../models/Contracts");
 const getContractById = async (req, res) => {
   try {
     const { id } = req.params;
+    console.log("Fetching contract for collaborator ID:", id);
     const contract = await Contract.findOne({ colaborador: id });
+    console.log("Contract found:", contract);
     if (!contract) {
       return res.status(404).json({ message: "Contrato no encontrado" });
     }
 
-    res.status(200).json(contract);
+    return res.status(200).json(contract);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };
 
